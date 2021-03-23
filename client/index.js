ed982de75c3d810a5ed974e2258b2f4e090c6ee4
Davi Reis Vieira de Souza <vieira08davi38@gmail.com>
2021-03-23 14:55:23 -0300

    Adicionando serverSide

--- client/index.js
+++ client/index.js
@@ -1,7 +1,19 @@
+/*
+node index.js --username davi_reis --room chat01 --hostUrl localhost
+*/
+
 import Events from "events";
+import CliConfig from "./src/cliConfig.js";
+import SocketClient from "./src/socket.js";
 import TerminalController from "./src/terminalController.js";
+
+const [nodePath, filePath, ...commands] = process.argv;
+const config = CliConfig.paseArguments(commands);
 
 const componentEmitter = new Events();
 
-const controller = new TerminalController();
-await controller.initializeTable(componentEmitter);
+const socketClient = new SocketClient(config);
+await socketClient.initialize();
+
+// const controller = new TerminalController();
+// await controller.initializeTable(componentEmitter);
